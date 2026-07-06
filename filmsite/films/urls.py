7c5d82f84238b3b556425a114cd914c52d9e482f
FieryFreak1 <azikbr006@gmail.com>
2026-07-06 13:42:34 +0300

    Update urls.py

--- filmsite/films/urls.py
+++ filmsite/films/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path('about/', views.about, name='about')
     path('films/', views.film_list, name='film_list'),
     path('search/', views.search_film, name='search_film'),
     path('films/<int:film_id>/', views.film_detail, name='film_detail'),
